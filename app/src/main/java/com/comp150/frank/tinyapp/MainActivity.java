93a2415079bdb51eb72406d0487e2703f28f1ca6
Frank Hattler <frank.hattler@tufts.edu>
2017-09-13 22:38:05 -0400

    Added image view with context dependent images

--- app/src/main/java/com/comp150/frank/tinyapp/MainActivity.java
+++ app/src/main/java/com/comp150/frank/tinyapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setHalligan(View v) {
-        Intent viewPic = new Intent(this, PictureViewer.class);
-        startActivity(viewPic);
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 1);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setLibrary(View v) {
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 2);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setGym(View v) {
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 3);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setDewick(View v) {
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 4);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setCarmichael(View v) {
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 5);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setHodgdon(View v) {
+        Intent intent = new Intent(MainActivity.this, PictureViewer.class);
+        Bundle b = new Bundle();
+        b.putInt("building", 6);
+        intent.putExtras(b);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/comp150/frank/tinyapp/PictureViewer.java
+++ app/src/main/java/com/comp150/frank/tinyapp/PictureViewer.java
@@ -1,7 +1,11 @@
 package com.comp150.frank.tinyapp;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class PictureViewer extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class PictureViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_viewer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle b = getIntent().getExtras();
+        int value = -1; // or other values
+        if(b != null)
+            value = b.getInt("building");
+
+        ImageView building = (ImageView)findViewById(R.id.buildingViewer);
+        switch (value) {
+            case 1: building.setImageResource(R.drawable.halligan);
+                break;
+            case 2: building.setImageResource(R.drawable.library);
+                break;
+            case 3: building.setImageResource(R.drawable.gym);
+                break;
+            case 4: building.setImageResource(R.drawable.dewick);
+                break;
+            case 5: building.setImageResource(R.drawable.carmichael);
+                break;
+            case 6: building.setImageResource(R.drawable.hodgdon);
+                break;
+        }
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
     }
 }
